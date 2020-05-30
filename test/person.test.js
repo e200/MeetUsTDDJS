0bf4bec369fcc1e24745f728535c562067c6e765
tandavala <jose.tandavala@gmail.com>
2020-05-30 17:15:03 +0100

    adding README.md

--- test/person.test.js
+++ test/person.test.js
@@ -1,18 +1,19 @@
-const Person = require('../src/person')
+const Person = require("../src/person");
 
-describe('Test class person', () => {
-  it('should test if a person have a first name, a last name and a full name', () => {
+describe("Test class person", () => {
+  it("should test if a person have a first name, a last name and a full name", () => {
     const person = new Person({
-      firstName: 'Eleandro',
-      lastName: 'Duzentos',
-    })
+      firstName: "Eleandro",
+      lastName: "Duzentos",
+    });
 
-    expect(person).toHaveProperty('firstName')
-    expect(person).toHaveProperty('lastName')
-    expect(person).toHaveProperty('fullName')
-    expect(person).toHaveProperty('genre')
-    expect(person.firstName).toBe('Eleandro')
-    expect(person.lastName).toBe('Duzentos')
-    expect(person.fullName).toBe('Eleandro Duzentos')
+    expect(person).toHaveProperty("firstName");
+    expect(person).toHaveProperty("lastName");
+    expect(person).toHaveProperty("fullName");
+    expect(person).toHaveProperty("genre");
+
+    expect(person.firstName).toBe("Eleandro");
+    expect(person.lastName).toBe("Duzentos");
+    expect(person.fullName).toBe("Eleandro Duzentos");
   });
 });
